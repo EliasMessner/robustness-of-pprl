c40fcc6a464d5a6753c54eaede1a40efa831124a
Elias Messner <em83zewo@studserv.uni-leipzig.de>
2022-11-14 19:34:47 +0100

    updated run.sh

--- RLModule/run.sh
+++ RLModule/run.sh
@@ -1,5 +1,5 @@
 # example:
-# java -cp .:lib/*:classes RLInterface.Main -d "../data/subsets_out/random_300_1_0.1.csv" -o "../data/matchings/random_300_1_0.1.csv" -l 1000 -k 10 -t 0.75
+# java -cp .:lib/*:classes RLInterface.Main -d "../data/dataset_variations/random_300_1_0.1.csv" -o "../data/matchings/random_300_1_0.1.csv" -l 1000 -k 10 -t 0.75
 
 # with arguments passed on script execution:
 java -cp .:lib/*:classes RLInterface.Main $*
