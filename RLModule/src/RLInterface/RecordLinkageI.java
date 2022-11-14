e1fdda2e5c24a9c01c6760ceb1a82f7a7c0ac621
Elias Messner <em83zewo@studserv.uni-leipzig.de>
2022-11-15 00:16:20 +0100

    updated RecordLinkageI to config file argument

--- RLModule/src/RLInterface/RecordLinkageI.java
+++ RLModule/src/RLInterface/RecordLinkageI.java
@@ -2,7 +2,7 @@ package RLInterface;
 
 public interface RecordLinkageI {
 
-    void readData(String fromFile, String... params);
+    void readData(String fromFile, String configFile);
 
     void getLinking(String outFile);
 
