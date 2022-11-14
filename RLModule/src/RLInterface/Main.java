2001dabf7a9c27b271e76592923e6399d85589b0
Elias Messner <em83zewo@studserv.uni-leipzig.de>
2022-11-15 00:15:07 +0100

    replaced l,k,t with config file argument

--- RLModule/src/RLInterface/Main.java
+++ RLModule/src/RLInterface/Main.java
@@ -4,21 +4,18 @@ import org.apache.commons.cli.*;
 
 public class Main {
 
-    static String fromFile, outFile;
-    // static String configFile;
-    static String l, k, t;
+    static String fromFile, outFile, configFile;
 
     /**
      * Required command line options:
-     * -d / -data: file path to dataset
-     * -o / -out: file path to out file (where linked pairs should be stored)
-     * -l, -k, -t
-     * TODO: l, k, t to be replaced with config file
+     * -d / -data: path to dataset
+     * -o / -out: path to out file (where linked pairs should be stored)
+     * -c / -config: path to config file
      */
     public static void main(String[] args) {
         tryGetCommandLineArgumentValues(args);
         PPRLAdapter adapter = new PPRLAdapter();
-        adapter.readData(fromFile, l, k, t);
+        adapter.readData(fromFile, configFile);
         adapter.getLinking(outFile);
     }
 
@@ -32,10 +29,7 @@ public class Main {
             CommandLine cmd = getCommandLine(args);
             fromFile = cmd.getOptionValue("d");
             outFile = cmd.getOptionValue("o");
-            // String configFile = cmd.getOptionValue("c");
-            l = cmd.getOptionValue("l");
-            k = cmd.getOptionValue("k");
-            t = cmd.getOptionValue("t");
+            configFile = cmd.getOptionValue("c");
         } catch (ParseException e) {
             System.err.print("Parse error: ");
             System.err.println(e.getMessage());
@@ -47,13 +41,7 @@ public class Main {
         Options options = new Options();
         options.addRequiredOption("d", "data", true, "Filepath to dataset used for linkage process.");
         options.addRequiredOption("o", "out", true, "Filepath to write linked pairs into.");
-        // options.addRequiredOption("c", "config", true, "Filepath to config json file.");
-        // TODO read Parameters from config file and add additional layer of abstraction,
-        //  i.e. this class should have no knowledge of l, k, t, but just pass the config
-        //  file to the adapter
-        options.addRequiredOption("l", "bflength", true, "Length of Bloom Filter.");
-        options.addRequiredOption("k", "hashcount", true, "Number of hashing iterations on Bloom Filter.");
-        options.addRequiredOption("t", "threshold", true, "Threshold for linking process.");
+        options.addRequiredOption("c", "config", true, "Filepath to config json file.");
         CommandLineParser parser = new DefaultParser();
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
         return parser.parse(options, args);
