7508cc14a4426e9d3e92d3dcf03999fec7cc5fc7
Elias Messner <em83zewo@studserv.uni-leipzig.de>
2022-11-15 21:57:49 +0100

    updated constants

--- MainModule/constants.py
+++ MainModule/constants.py
@@ -1,6 +1,5 @@
 exp_config_path = "../data/exp_config.json"
-run_configs_path = "../data/run_configs/"
-base_dataset_path = "data/2021_NCVR_Panse_001/dataset_ncvr_dirty.csv"
-col_names = ["sourceID", "globalID", "localID", "FIRSTNAME", "MIDDLENAME", "LASTNAME", "YEAROFBIRTH", "PLACEOFBIRTH", "COUNTRY", "CITY", "PLZ", "STREET", "GENDER", "ETHNIC", "RACE"]
+exp_config_temp_path = "../data/exp_config_temp.json"
+dm_config_path = "../data/dataset_modifier.json"
 dataset_variations_dir = "../data/dataset_variations"
 matchings_dir = "../data/matchings"
