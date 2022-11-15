7413efb820b6568aa21035465122ceb66a0c5b3f
Elias Messner <em83zewo@studserv.uni-leipzig.de>
2022-11-15 20:15:43 +0100

    constants.col_names as literal string list

--- MainModule/constants.py
+++ MainModule/constants.py
@@ -1,7 +1,6 @@
 exp_config_path = "../data/exp_config.json"
 run_configs_path = "../data/run_configs/"
 base_dataset_path = "data/2021_NCVR_Panse_001/dataset_ncvr_dirty.csv"
-col_names = "sourceID,globalID,localID,FIRSTNAME,MIDDLENAME,LASTNAME,YEAROFBIRTH,PLACEOFBIRTH,COUNTRY,CITY,PLZ," \
-            "STREET,GENDER,ETHNIC,RACE".split(",")
+col_names = ["sourceID", "globalID", "localID", "FIRSTNAME", "MIDDLENAME", "LASTNAME", "YEAROFBIRTH", "PLACEOFBIRTH", "COUNTRY", "CITY", "PLZ", "STREET", "GENDER", "ETHNIC", "RACE"]
 dataset_variations_dir = "../data/dataset_variations"
 matchings_dir = "../data/matchings"
