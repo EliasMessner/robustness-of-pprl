2fd8eb9da7cd13b9a21f7d37c785615d50d8c605
Elias Messner <em83zewo@studserv.uni-leipzig.de>
2022-11-16 13:51:41 +0100

    Changed t.equals(null) to t == null.

--- RLModule/src/PPRL/Util.java
+++ RLModule/src/PPRL/Util.java
@@ -7,13 +7,13 @@ public class Util {
 
     public static <T> void checkIfNoneNull(T[] array) {
         for (T t : array) {
-            if (t.equals(null)) throw new NullPointerException();
+            if (t == null) throw new NullPointerException();
         }
     }
 
     public static <T> void checkIfNoneNull(Iterable<T> iterable) {
         for (T t : iterable) {
-            if (t.equals(null)) throw new NullPointerException();
+            if (t == null) throw new NullPointerException();
         }
     }
 
