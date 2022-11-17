c68d0c8056586663916b5f18c03fec8ec8ae2137
Elias Messner <em83zewo@studserv.uni-leipzig.de>
2022-11-17 22:50:47 +0100

    Made eval adapter a class and added metrics

--- MainModule/eval_adapter.py
+++ MainModule/eval_adapter.py
@@ -1,22 +1,33 @@
 import pandas as pd
+import recordlinkage as rl
 
 
-def prepare_for_eval(data_path, data_clm_names, pred_path,
-                     id_clm="globalID", src_clm="sourceID",
-                     pred_clm_a="globalID_A", pred_clm_b="globalID_B") -> (pd.DataFrame, pd.DataFrame, int):
-    """
-    Read dataset and predicted matches from files, generate true matches from data,
-    return dataframes ready for evaluation with third party recordlinkage module.
-    https://recordlinkage.readthedocs.io/en/latest/ref-evaluation.html
-    :returns: (links_true, links_pred, total)
-    total is the total number of possible record pairs, aka. product of the sizes of the two data sources
-    """
-    data = pd.read_csv(data_path, names=data_clm_names)
-    d1, d2 = [x for _, x in data.groupby(src_clm)]
-    ids_true = pd.merge(d1, d2, how="inner", on=[id_clm])[id_clm]
-    links_true = pd.concat([ids_true, ids_true], axis=1)
-    links_true.columns = [pred_clm_a, pred_clm_b]
-    links_true.set_index([pred_clm_a, pred_clm_b], inplace=True)
-    links_pred = pd.read_csv(pred_path, names=[pred_clm_a, pred_clm_b], index_col=[pred_clm_a, pred_clm_b])
-    total = d1.shape[0] * d2.shape[0]
-    return links_true, links_pred, total
+class EvalAdapter:
+    def __init__(self, data_path, data_clm_names, pred_path,
+                 id_clm="globalID", src_clm="sourceID",
+                 pred_clm_a="globalID_A", pred_clm_b="globalID_B") -> (pd.DataFrame, pd.DataFrame, int):
+        """
+        Read dataset and predicted matches from files, generate true matches from data,
+        prepare the data needed for evaluation with third party recordlinkage module.
+        https://recordlinkage.readthedocs.io/en/latest/ref-evaluation.html
+        """
+        data = pd.read_csv(data_path, names=data_clm_names)
+        d1, d2 = [x for _, x in data.groupby(src_clm)]
+        ids_true = pd.merge(d1, d2, how="inner", on=[id_clm])[id_clm]
+        self.links_true = pd.concat([ids_true, ids_true], axis=1)
+        self.links_true.columns = [pred_clm_a, pred_clm_b]
+        self.links_true.set_index([pred_clm_a, pred_clm_b], inplace=True)
+        self.links_pred = pd.read_csv(pred_path, names=[pred_clm_a, pred_clm_b], index_col=[pred_clm_a, pred_clm_b])
+        self.total = d1.shape[0] * d2.shape[0]  # total number of possible record pairs
+
+    def precision(self):
+        return rl.precision(self.links_true, self.links_pred)
+
+    def recall(self):
+        return rl.recall(self.links_true, self.links_pred)
+
+    def fscore(self):
+        return rl.fscore(self.links_true, self.links_pred)
+
+    def accuracy(self):
+        return rl.accuracy(self.links_true, self.links_pred, self.total)
--- MainModule/test_eval_adapter.py
+++ MainModule/test_eval_adapter.py
@@ -1,8 +1,7 @@
 from unittest import TestCase
 import pandas as pd
-import recordlinkage as rl
 
-from eval_adapter import prepare_for_eval
+from eval_adapter import EvalAdapter
 
 
 class Test(TestCase):
@@ -10,36 +9,27 @@ class Test(TestCase):
         self.data_path = "data/test_eval_data.csv"
         self.data_clm_names = ["sourceID", "globalID"]
         self.pred_path = "data/test_eval_pred.csv"
+        self.eval = EvalAdapter(self.data_path, self.data_clm_names, self.pred_path)
 
-    def test_prepare_for_eval(self):
-        links_true, links_pred, total = prepare_for_eval(self.data_path, self.data_clm_names, self.pred_path)
-        pd.testing.assert_frame_equal(links_true, pd.DataFrame.from_dict({
+    def test_init(self):
+        pd.testing.assert_frame_equal(self.eval.links_true, pd.DataFrame.from_dict({
             "globalID_A": [3, 4],
             "globalID_B": [3, 4]
         }).set_index(["globalID_A", "globalID_B"]))
-        pd.testing.assert_frame_equal(links_pred, pd.DataFrame.from_dict({
+        pd.testing.assert_frame_equal(self.eval.links_pred, pd.DataFrame.from_dict({
             "globalID_A": [1, 3, 2],
             "globalID_B": [4, 3, 5]
         }).set_index(["globalID_A", "globalID_B"]))
-        self.assertEqual(total, 16)
+        self.assertEqual(self.eval.total, 16)
 
-    def test_metrics(self):
-        links_true, links_pred, total = prepare_for_eval(self.data_path, self.data_clm_names, self.pred_path)
-        exp_tp = 1
-        exp_fp = 2
-        exp_tn = 12
-        exp_fn = 1
-        exp_p = 1/3
-        exp_r = 1/2
-        obs_tp = rl.true_positives(links_true, links_pred)
-        obs_fp = rl.false_positives(links_true, links_pred)
-        obs_tn = rl.true_negatives(links_true, links_pred, total)
-        obs_fn = rl.false_negatives(links_true, links_pred)
-        obs_p = rl.precision(links_true, links_pred)
-        obs_r = rl.recall(links_true, links_pred)
-        self.assertEqual(exp_tp, obs_tp)
-        self.assertEqual(exp_fp, obs_fp)
-        self.assertEqual(exp_tn, obs_tn)
-        self.assertEqual(exp_fn, obs_fn)
-        self.assertEqual(exp_p, obs_p)
-        self.assertEqual(exp_r, obs_r)
+    def test_precision(self):
+        self.assertEqual(1/3, self.eval.precision())
+
+    def test_recall(self):
+        self.assertEqual(1/2, self.eval.recall())
+
+    def test_fscore(self):
+        self.assertEqual(2/5, self.eval.fscore())
+
+    def test_accuracy(self):
+        self.assertEqual(13/16, self.eval.accuracy())
