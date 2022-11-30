fb1451eeed42d1e614d4e62018015fed6f7adb0a
EliasMessner <elias151019@gmail.com>
2022-11-30 10:29:12 +0100

    Added logging and passing storage file path

--- MainModule/constants.py
+++ MainModule/constants.py
@@ -1,4 +1,9 @@
+import os
+
 exp_config_path = "data/exp_config.json"
 dm_config_path = "data/dataset_modifier.json"
 dataset_variants_dir = "data/dataset_variants"
 matchings_dir = "data/matchings"
+logs_dir = "logs"
+
+pprl_storage_file_location = os.path.abspath(os.path.join("..", "RLModule", "storage", "pbm"))
