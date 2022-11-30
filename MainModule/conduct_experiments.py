311bb22ec3d791ba75075db3c460c32ca2ef928e
EliasMessner <elias151019@gmail.com>
2022-11-30 16:37:44 +0100

    Removed colons from log file name for windows compatibility

--- MainModule/conduct_experiments.py
+++ MainModule/conduct_experiments.py
@@ -43,7 +43,7 @@ def resolve_seed_lists(experiments):
 
 def prepare_logger():
     Path(logs_dir).mkdir(parents=True, exist_ok=True)
-    timestamp = dt.now().strftime("%Y-%m-%d_%H:%M:%S")
+    timestamp = dt.now().strftime("%Y-%m-%d_%H-%M-%S")
     logging.basicConfig(filename=os.path.join(logs_dir, f"{timestamp}.txt"),
                         filemode='a',
                         format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
