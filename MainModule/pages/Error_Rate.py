c76585824c97e0e90237f73b9a16f28b92c2675e
EliasMessner <elias151019@gmail.com>
2023-02-23 18:39:45 +0100

    Added streamlit files.

---
+++ MainModule/pages/Error_Rate.py
@@ -0,0 +1,46 @@
+import itertools
+from streamlit_entry import *
+
+st.set_page_config(page_title="Error Rate")
+st.write("# Error Rate")
+
+CHART_ARRANGEMENT = st.sidebar.radio(label="Chart Arrangement", options=["Vertical", "Horizontal"])
+
+exp_ids = experiment_multiselect(default=["762972457522318676"])
+runs = get_runs(exp_ids)
+assert (runs["params.subset_selection"] == "ERROR_RATE").all()
+
+step_selections = st.multiselect("Step", options=[2, 3], default=[2, 3])
+preserve_overlap_selections = st.multiselect("Preserve Overlap", options=["True", "False"], default=["True", "False"])
+downsampling_selections = st.multiselect("Downsampling", options=DOWNSAMPLING_OPTIONS, default=["TO_MIN_GROUP_SIZE"])
+
+
+def resolve_downsampling(observed_value, selected_value):
+    if selected_value == "Exact Value":
+        return observed_value.isdigit()
+    return observed_value == selected_value
+
+
+for step, preserve_overlap, downsampling in itertools.product(step_selections, preserve_overlap_selections, downsampling_selections):
+    # TODO add step as param to dm_config and select by this param
+    if step == 2:
+        ranges = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 12]]
+    elif step == 3:
+        ranges = [[0, 2], [3, 5], [6, 8], [9, 12]]  # TODO change to [[1, 3], [4, 6], [7, 10]]
+    else:
+        raise ValueError()
+    param = "params.range"
+    runs_filtered = runs[
+        (runs[param].isin([str(r) for r in ranges]))
+        & (runs["params.preserve_overlap"] == str(preserve_overlap))
+        & (runs["params.downsampling"].apply(lambda ds: resolve_downsampling(ds, downsampling)))
+        ]
+    with st.expander(f"Step={step} | PO={preserve_overlap} | downsampling={downsampling} -> *{len(runs_filtered)} Runs*"):
+        st.write(f"Step = **{step}** | "
+                 f"Preserve Overlap = **{preserve_overlap}** | "
+                 f"Downsamplng = **{downsampling}**")
+        if runs_filtered.shape[0] == 0:
+            continue
+        # st.dataframe(runs_filtered)
+        basic_box_plot(runs_filtered, param=param, x_order=sorted(runs_filtered[param].unique().tolist()),
+                       chart_arrangement=CHART_ARRANGEMENT)
