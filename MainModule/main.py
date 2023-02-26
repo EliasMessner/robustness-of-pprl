235af3408d574400c5ec0f8975a84a5087d6f376
EliasMessner <elias151019@gmail.com>
2023-02-26 16:41:19 +0100

    Renamed evaluator.py from evaluate.py.

--- MainModule/main.py
+++ MainModule/main.py
@@ -13,7 +13,7 @@ def main():
 def run(dm, ec):
     sp.run(f"python dataset_modifier.py data/{dm}.json", shell=True, check=True)
     sp.run(f"python conduct_matching.py data/{ec}.json", shell=True, check=True)
-    sp.run("python evaluate.py", shell=True, check=True)
+    sp.run("python evaluator.py", shell=True, check=True)
 
 
 if __name__ == "__main__":
