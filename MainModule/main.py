37620c65cb3d7ec5ba7c507d217ffee85a87f627
EliasMessner <elias151019@gmail.com>
2023-02-27 18:26:57 +0100

    Prepared all config files and added all to main.py

--- MainModule/main.py
+++ MainModule/main.py
@@ -8,6 +8,10 @@ import subprocess as sp
 def main():
     run("dm_config_error_rate", "rl_config")
     run("dm_config_attr_val_length", "rl_config")
+    run("dm_config_gender", "rl_config")
+    run("dm_config_age", "rl_config")
+    run("dm_config_plz", "rl_config")
+    # run("dm_config_random_cartesian_product", "rl_config_10_seeds")  TODO run this over night
 
 
 def run(dm_config_name, rl_config_name):
