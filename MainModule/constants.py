92860f8d2dd1c3b2c294b6993838ec2bbfb31836
EliasMessner <elias151019@gmail.com>
2023-03-16 16:56:16 +0100

    Renamed rl config to rl param variation to avoid confusion with rl base config.

--- MainModule/constants.py
+++ MainModule/constants.py
@@ -6,5 +6,6 @@ dataset_variants_dir = "data/dataset_variants"
 dataset_variants_dir_test = "test_resources/dataset_variants_test"
 matchings_dir = "data/matchings"
 logs_dir = "logs"
+rl_params_file_name = "rl_params.json"
 
 pprl_storage_file_location = os.path.abspath(os.path.join("..", "RLModule", "storage", "pbm"))
--- MainModule/create_matching.py
+++ MainModule/create_matching.py
@@ -7,7 +7,8 @@ import logging
 
 from tqdm import tqdm
 
-from constants import matchings_dir, default_rl_config_path, dataset_variants_dir, pprl_storage_file_location
+from constants import matchings_dir, default_rl_config_path, dataset_variants_dir, pprl_storage_file_location, \
+    rl_params_file_name
 from log import prepare_logger
 from util import get_config_path_from_argv, read_json, write_json, list_folder_names_flattened
 
@@ -23,56 +24,56 @@ def main(rl_base_config_path=None):
     # copy rl_base_config matchings dir on top level, so it can be logged as artifact later
     shutil.copyfile(rl_base_config_path, os.path.join(matchings_dir, "rl_base_config.json"))
     print(f"Conducting matching based on {rl_base_config_path}")
-    iterate_rl_configs(rl_base_config_path)
+    iterate_rl_param_variations(rl_base_config_path)
 
 
-def iterate_rl_configs(rl_base_config_path):
-    rl_configs = resolve_rl_config(read_json(rl_base_config_path))
-    for rl_config_no, rl_config in tqdm(list(enumerate(rl_configs)), desc="RL-configs"):
-        rl_config_folder = os.path.join(matchings_dir, f"rl_config_{rl_config_no}")
-        Path(rl_config_folder).mkdir(exist_ok=True, parents=True)
-        iterate_variants(rl_config, rl_config_folder)
+def iterate_rl_param_variations(rl_base_config_path):
+    rl_param_variations = resolve_rl_config(read_json(rl_base_config_path))
+    for rl_params_no, rl_params in tqdm(list(enumerate(rl_param_variations)), desc="RL-params"):
+        rl_params_folder = os.path.join(matchings_dir, f"rl_params_{rl_params_no}")
+        Path(rl_params_folder).mkdir(exist_ok=True, parents=True)
+        iterate_variants(rl_params, rl_params_folder)
 
 
 def resolve_rl_config(rl_base_config: dict):
     """
-    Resolve the rl base config into the individual rl configurations.
-    E.g., in case there is a list of seeds (=token-salting affixes) given, make one rl-config for each value in
+    Resolve the rl base config into the individual rl param variations.
+    E.g., in case there is a list of seeds (=token-salting affixes) given, make one rl-param variation for each value in
     the list.
     """
     rl_base_config_val_lists = {key: value if isinstance(value, list) else [value]
                                 for key, value in
                                 rl_base_config.items()}  # convert all values to lists if they aren't already
-    rl_configs = []
+    rl_param_vars = []
     for value_combination in itertools.product(*rl_base_config_val_lists.values()):
-        new_rl_config = {key: value for key, value in zip(rl_base_config_val_lists.keys(), value_combination)}
-        rl_configs.append(new_rl_config)
-    return rl_configs
+        new_rl_param_var = {key: value for key, value in zip(rl_base_config_val_lists.keys(), value_combination)}
+        rl_param_vars.append(new_rl_param_var)
+    return rl_param_vars
 
 
-def iterate_variants(rl_config, rl_config_folder):
-    rl_config_path = write_rl_config(rl_config, rl_config_folder)
+def iterate_variants(rl_params, rl_params_folder):
+    rl_params_file_path = write_rl_params_file(rl_params, rl_params_folder)
     for variant_name in tqdm(list_folder_names_flattened(dataset_variants_dir), desc="Variants", leave=False):
         data_path = os.path.join(dataset_variants_dir, variant_name, "records.csv")
-        outfile_path = os.path.join(rl_config_folder, variant_name, "matching.csv")
-        call_rl_module(data_path, outfile_path, rl_config_path)
+        outfile_path = os.path.join(rl_params_folder, variant_name, "matching.csv")
+        call_rl_module(data_path, outfile_path, rl_params_file_path)
 
 
-def write_rl_config(rl_config: dict, out_folder: str) -> str:
+def write_rl_params_file(rl_params: dict, out_folder: str) -> str:
     """
-    create config for rl module for this run and return its path
+    create JSON file with params for rl module for this run and return its path
     """
     Path(out_folder).mkdir(exist_ok=True, parents=True)
-    rl_config_path = os.path.join(out_folder, "rl_config.json")
-    write_json(rl_config, rl_config_path)
-    return rl_config_path
+    rl_params_path = os.path.join(out_folder, rl_params_file_name)
+    write_json(rl_params, rl_params_path)
+    return rl_params_path
 
 
-def call_rl_module(data_path, outfile_path, rl_config_path):
+def call_rl_module(data_path, outfile_path, rl_params_path):
     cmd = ["java", "-jar", "../RLModule/target/RLModule.jar",
            "-d", data_path,
            "-o", outfile_path,
-           "-c", rl_config_path,
+           "-c", rl_params_path,
            "-s", pprl_storage_file_location]
     try:
         output = subprocess.check_output(cmd, encoding='UTF-8')
