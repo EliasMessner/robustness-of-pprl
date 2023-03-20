31ae4632e1c9fc0002008a2c84b1691dbaf7d0a3
EliasMessner <elias151019@gmail.com>
2023-03-20 22:25:13 +0100

    delete old mlruns

--- MainModule/error_rates.py
+++ MainModule/error_rates.py
@@ -1,5 +1,10 @@
+from typing import Union
+
 import numpy as np
 import nltk
+import pandas as pd
+
+from dataset_properties import split_by_source_id
 
 
 def count_errors(row, attrs, suffixes=None):
@@ -19,3 +24,11 @@ def avg_edit_distance(row, attrs, suffixes=None):
         [nltk.edit_distance(str(row[attr + suffixes[0]]), str(row[attr + suffixes[1]]))
          for attr in attrs]
     )
+
+
+def get_all_errors(df: pd.DataFrame, measure: Union[callable, str], global_id_col_name="globalID") -> pd.Series:
+    measure = globals()[measure] if isinstance(measure, str) else measure  # resolve method name if given as string
+    df_a, df_b = split_by_source_id(df)
+    pairs = df_a.merge(df_b, on=global_id_col_name, suffixes=["_a", "_b"])
+    attrs = [col for col in df.columns.values if not col.lower().endswith("id")]
+    return pairs.apply(lambda row: measure(row, attrs), axis=1)
